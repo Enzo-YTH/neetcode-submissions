205a88b2931c2d4b0f95d4c1779fa255d80a066b
Enzo-YTH <yentse.hsieh@gmail.com>
2026-07-25 15:59:35 +0800

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,24 @@
+class MinStack {
+private:
+    vector<int> stack;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        stack.push_back(val);
+    }
+    
+    void pop() {
+        stack.pop_back();
+    }
+    
+    int top() {
+        return stack.back();
+    }
+    
+    int getMin() {
+        return *std::min_element(stack.begin(), stack.end());
+    }
+};
